1fcc9d4596112b5751495263a70ef183b22f67e6
Princess Itaya <taycornelius@att.net>
2020-03-16 13:40:54 -0400

    css and passing through data

--- cart.php
+++ cart.php
@@ -12,7 +12,7 @@
         <div class="container">
         <?php 
 
-        $connect = msqli_connect('localhost', 'root', 'root', 'cart');
+        $connect = mysqli_connect('localhost', 'root', 'root', 'cart');
         $query = 'SELECT * FROM products ORDER by id ASC';
 
         $result = mysqli_query($connect, $query);
@@ -23,7 +23,18 @@
                 print_r($product);
                 ?>
                 <div class="col-sm-4 col-md-3">
-                <form method="post" action="index.php?action =add&id=<?php echo $product['id']; ?> ">"
+                <form method="post" action="index.php?action =add&id=<?php echo $product['id']; ?> ">
+                <div class="products">
+                <img src="<?php echo $product['image']; ?>" class="img-responsive"/>
+                <h4 class="text-info"><?php echo $product['name']; ?></h4>
+                <h4>$ <?php echo $product['price']; ?></h4>
+                <input type="text" name="quantity" class="form-control" value="1" />
+                <input type="hidden" name="name" value="<?php echo $product['name'] ?>"/>
+                <input type="hidden" name="price" value="<?php echo $product['price'] ?>"/>
+                <input type="submit" name="add-to-cart" style="margin-top:5px;" class="btn btn-info"
+                            value="Add to Cart" />
+                </div>
+                </form>
 
                 </div>
                  <?php 
